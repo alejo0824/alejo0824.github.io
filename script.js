fb396ae37ee41873ee2fb44554fa22df4b776300
Alejandro Tellez <alejo082495@gmail.com>
2024-02-29 23:50:38 -0500

    Se agrega el botón <copiar> para copiar el resultado, ya sea para desencriptar o encriptar, se agrega nueva validación donde el usuario no pueda ingresar caracteres especiales

--- script.js
+++ script.js
@@ -1,120 +1,160 @@
-document.addEventListener('DOMContentLoaded', function(){ 
-  
+document.addEventListener("DOMContentLoaded", function () {
   // Seleccionar los elementos de la interfaz
-  const textoEncriptado = document.querySelector('#textoEncriptado');
-  const formulario = document.querySelector('#formulario')
-  const btnEncriptar = document.querySelector('#encriptar')
-  const btndesencriptar = document.querySelector('#desencriptar')
-  const resultado = document.querySelector('#resultado')
+  const textoEncriptado = document.querySelector("#textoEncriptado");
+  const formulario = document.querySelector("#formulario");
+  const btnEncriptar = document.querySelector("#encriptar");
+  const btndesencriptar = document.querySelector("#desencriptar");
+  const divResultado = document.querySelector(".desencriptador");
 
-  //Asignar Eventos 
-  // textoEncriptado.addEventListener('blur',validar);
-  btnEncriptar.addEventListener('click',validar)
-  btndesencriptar.addEventListener('click',validar)
+  //Asignar Eventos
+  btnEncriptar.addEventListener("click", validar);
+  btndesencriptar.addEventListener("click", validar);
 
-  function validar(e){
+  function validar(e) {
+    let resultado = "";
+    let textoUsuario = textoEncriptado.value.trim().toLowerCase();
+    // Expresión regular que acepta solo letras (mayúsculas y minúsculas), números y espacios
+    let expresionRegular = /^[a-zA-Z0-9\s]+$/;
+
+    // Quitamos las opciones que vienen por defecto del evento  clic en un boton
     e.preventDefault();
 
-    if(textoEncriptado.value.trim().toLowerCase() === ''){
-      mostrarAlerta('Por favor ingresa texto');
+    // Validación si el usuario no ingresa texto y da clic en alguna opción
+    if (textoUsuario === "") {
+      mostrarAlerta("Por favor ingresa texto");
       return;
     }
+
+    // Verifica si el texto cumple con la expresión regular
+    if (!expresionRegular.test(textoUsuario)) {
+      mostrarAlerta("No se permite ingresar caracteres especiales");
+      return;
+    }
+
+    //Eliminamos las alertas si existieron
     limpiarAlerta();
-    let texto = textoEncriptado.value
-    //textoEncriptado.value = ''; //Descomentar par PRD
+    textoEncriptado.value = "";
 
-    encriptarODesencriptar = e.target.value.toLowerCase()
+    encriptarODesencriptar = e.target.value.toLowerCase();
 
-    if (encriptarODesencriptar == 'encriptar'){
-      encriptar(texto);
+    if (encriptarODesencriptar == "encriptar") {
+      resultado = encriptar(textoUsuario);
+    } else {
+      resultado = desencriptar(textoUsuario);
     }
-    else {
-      desencriptar(texto);
-    }
+
+    mostrarResultado(resultado);
   }
 
-  function mostrarAlerta(mensaje){
+  function mostrarAlerta(mensaje) {
     // Comprobar si ya existe una alerta
     limpiarAlerta();
 
-    // Generar alerta HTML 
-    const error = document.createElement('P')
+    // Generar alerta HTML
+    const error = document.createElement("P");
     error.textContent = mensaje;
-    error.classList.add('error')
+    error.classList.add("error");
 
     // Inyectar el error al formulario
-    formulario.appendChild(error)
+    formulario.appendChild(error);
   }
 
-  function limpiarAlerta(){
-    const alerta = document.querySelector('.error')
-    if(alerta){
+  function limpiarAlerta() {
+    const alerta = document.querySelector(".error");
+    if (alerta) {
       alerta.remove();
     }
   }
 
-  function encriptar(texto){
-    let textoEncriptado = '';
+  function encriptar(texto) {
+    let textoEncriptado = "";
 
     for (let i = 0; i < texto.length; i++) {
       let letra = texto[i];
 
       switch (letra) {
-        case 'a':
-          textoEncriptado += 'ai'
+        case "a":
+          textoEncriptado += "ai";
           break;
 
-        case 'e':
-          textoEncriptado += 'enter'
+        case "e":
+          textoEncriptado += "enter";
           break;
 
-        case 'i':
-          textoEncriptado += 'imes'
+        case "i":
+          textoEncriptado += "imes";
           break;
 
-        case 'o':
-          textoEncriptado += 'ober'
+        case "o":
+          textoEncriptado += "ober";
           break;
 
-        case 'u':
-          textoEncriptado += 'ufat'
+        case "u":
+          textoEncriptado += "ufat";
           break;
-      
+
         default:
-          textoEncriptado += letra
+          textoEncriptado += letra;
           break;
       }
     }
 
-    console.log(textoEncriptado);
+    return textoEncriptado;
   }
 
   function desencriptar(texto) {
-
     for (let i = 0; i < texto.length; i++) {
-      
-      if (texto.includes('ai')) {
-        texto = texto.replace('ai', 'a')
-    
-      } else if (texto.includes('enter')){
-        texto = texto.replace('enter', 'ae')
-    
-      } else if (texto.includes('imes')){
-        texto = texto.replace('imes', 'i')
-    
-      } else if (texto.includes('ober')){
-        texto = texto.replace('ober', 'o')
-    
-      } else if (texto.includes('ufat')){
-        texto = texto.replace('ufat', 'u')  
-      } 
+      if (texto.includes("ai")) {
+        texto = texto.replace("ai", "a");
+      } else if (texto.includes("enter")) {
+        texto = texto.replace("enter", "e");
+      } else if (texto.includes("imes")) {
+        texto = texto.replace("imes", "i");
+      } else if (texto.includes("ober")) {
+        texto = texto.replace("ober", "o");
+      } else if (texto.includes("ufat")) {
+        texto = texto.replace("ufat", "u");
+      }
     }
-    
-    console.log(texto);
+
+    return texto;
   }
 
-  function name(params) {
-    
+  function mostrarResultado(texto) {
+    const parrafoResultado = document.createElement("H4");
+    const inputCopiar = document.createElement("input");
+
+    // Agragando el resultado
+    divResultado.innerHTML = "";
+    parrafoResultado.textContent = texto;
+    parrafoResultado.classList.add("resultado");
+    divResultado.appendChild(parrafoResultado);
+
+    //Agragando el Botón para copiar
+    inputCopiar.type = "submit";
+    inputCopiar.value = "Copiar";
+    inputCopiar.classList.add("boton", "btn-desencriptar");
+    divResultado.appendChild(inputCopiar);
+
+    inputCopiar.addEventListener("click", function () {
+      let inputTemp = document.createElement("input");
+
+      // Asigna el texto al valor del input
+      inputTemp.value = texto;
+
+      // Agrega el input al DOM
+      document.body.appendChild(inputTemp);
+
+      // Selecciona el texto dentro del input
+      inputTemp.select();
+
+      // Ejecuta el comando "copy" para copiar el texto seleccionado
+      document.execCommand("copy");
+
+      // Remueve el input temporal del DOM
+      document.body.removeChild(inputTemp);
+
+      alert("Texto copiado");
+    });
   }
 });
-
